d9816637ec1e019f44d23fb08e67dd701431d219
Lewis <lewiswilson1998@hotmail.co.uk>
2019-10-17 17:28:14 +0100

    Fuzzy Logic layout

---
+++ Biometric Project/Assets/Scripts/FuzzyLogic/FuzzyModule.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuzzyModule : MonoBehaviour
+{
+    enum Desirability { Undersirable, Desirable, VeryDesirable };
+
+   
+}
+
+// setup of map of fuzzy variables? map(string, fuzzyVariable*)
+
+// DefuzzifyType either max_AV or centroid; (enum)
+// cross sections required for smapling centroid; (double check about centroid calculation) 
+
+// map of all fuzzy varaibles this module uses
+// map(string, fuzzyVariable) m_variables; 
+
+// vector containing all fuzzy rules
+// vector<FuzzyRule> m_Rules; 
+
+// zero the DOMs of consequents of each rule. Used by defuzzify()? 
+
+// create new "empty" fuzzy variable
+// fuzzyVariable CreateFLV(string VarName); 
+
+// Add rule to module
+// void AddRule(FuzzyTerm input, FuzzyTerm output) 
+
+// Fuzzy function 
+// void Fuzzify(string NameOfFLV, double value); 
+
+// given a fuzzy variable and a defuzzification method this returns a crisp value
+// double Defuzzify(const string key, DefuzzifyType method); 
---
+++ Biometric Project/Assets/Scripts/FuzzyLogic/FuzzySet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuzzySet : MonoBehaviour
+{
+    // Vector of membership functions (animation curves) 
+    public AnimationCurve[] membershipFunctions; 
+
+    private void Start()
+    {
+        //membershipFunctions[0].AddKey(0, 0);
+        //membershipFunctions[0].AddKey(0.5f, 1);
+        //membershipFunctions[0].AddKey(1, 0);
+    }
+
+    // Potential function to create fuzzy set curves via code parameters? 
+    // void SetTriangle(float left, float peak, float right); 
+
+    /* void Set(T linguisticVariable)
+     * this.Set(
+     */
+    
+}
