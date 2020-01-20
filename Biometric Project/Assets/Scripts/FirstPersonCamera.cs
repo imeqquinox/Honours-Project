b04a6612c8f18b37255fc1791c41ba5201142471
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2020-01-20 06:40:55 +0000

    Level layout and player controls

---
+++ Biometric Project/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    private float speed_hor = 2.0f;
+    private float speed_vert = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    private void Update()
+    {
+        yaw += speed_hor * Input.GetAxis("Mouse X");
+        pitch -= speed_vert * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f); 
+    }
+}
---
+++ Biometric Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController character_controller;
+
+    private float speed = 6.0f;
+    private float jump_speed = 8.0f;
+    private float gravity = 20.0f;
+
+    private Vector3 move_direction = Vector3.zero; 
+
+    private void Start()
+    {
+        character_controller = GetComponent<CharacterController>(); 
+    }
+
+    private void Update()
+    {
+        if (character_controller.isGrounded)
+        {
+            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            move_direction *= speed;
+            move_direction = Camera.main.transform.TransformDirection(move_direction); 
+
+            if (Input.GetButton("Jump"))
+            {
+                move_direction.y = jump_speed; 
+            }
+        }
+
+        move_direction.y -= gravity * Time.deltaTime;
+
+        character_controller.Move(move_direction * Time.deltaTime); 
+    }
+}
