774666a063b382358650193e6efceac8e9952fae
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2020-02-04 17:26:47 +0000

    NavMesh Update + Props

---
+++ Biometric Project/Assets/Scripts/AgentLinkMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentLinkMover : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private IEnumerator Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoTraverseOffMeshLink = false; 
+
+        while (true)
+        {
+            if (agent.isOnOffMeshLink)
+            {
+                yield return Walk();
+
+                //agent.isStopped = true; 
+                agent.CompleteOffMeshLink(); 
+            }
+            yield return null; 
+        }
+    }
+
+    private IEnumerator Walk()
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        while (agent.transform.position != endPos)
+        {
+            agent.transform.position = Vector3.MoveTowards(agent.transform.position, endPos, agent.speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
